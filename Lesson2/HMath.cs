d6c76ea33d7394d664a5c24515f5a7e1b6b128ce
25301214 <25301214@K216W20>
2026-09-09 09:21:15 +0500

    Добавьте файлы проекта.

---
+++ Lesson2/HMath.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson2
+{
+    internal class HMath
+    {
+        int age = 5;
+        float height = 20.5f;
+        string Name;
+        Class207.Math ClsTest = new Class207.Math();
+
+        void test1()
+        {
+            ClsTest.sum(3, 4, 5);
+        }
+    }
+}
