c10eccfda19133702cd621b8233b846f3cf204a6
Лерочка <safina_lerochka@bk.ru>
2026-09-09 12:27:48 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите первое число");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число");
+string input = Console.ReadLine();
+int num2 = int.Parse(input);
+Console.WriteLine("Введите знак");
+string sign = Console.ReadLine();
+
+if (sign == "+")
+    Console.WriteLine(num1 + num2);
+else if (sign == "-")
+    Console.WriteLine(num1 - num2);
+else if (sign == "*")
+    Console.WriteLine(num1 * num2);
+else if (sign == "/")
+    Console.WriteLine(num1 / num2);
+else
+    Console.WriteLine("Ошибка");
